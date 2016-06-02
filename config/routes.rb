022db8725e76b7acecd4fba4cfaaca898fc0a992
Allyson Ooi <allysonooi28@gmail.com>
2016-06-02 19:07:41 +0800

    Change name of view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static#index'
-
+  get 'admin/home'
   post 'static/ajax_submit_email'
 
   # Example of regular route:
